58d33ada9e50856524c23583041091e6beb2ec21
dnirns <daniel.irons@gmail.com>
2020-12-30 15:18:19 +0000

    dice and letter basics

---
+++ src/components/Dice.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+const Dice = () => {
+  const [dice1, setDice1] = useState(null)
+  const [dice2, setDice2] = useState(null)
+  const [bounce, setBounce] = useState('')
+
+  const rollOneDice = (min, max) => {
+    setBounce('')
+    setDice2(null)
+    const startTime = new Date().getTime()
+    const interval = setInterval(() => {
+      if (new Date().getTime() - startTime > 1500) {
+        clearInterval(interval)
+        return
+      }
+      setDice1(Math.floor(Math.random() * (max - min + 1)) + min)
+      setBounce('animate__animated animate__bounce')
+    }, 100)
+  }
+
+  const rollTwoDice = (min, max) => {
+    setBounce('')
+    const startTime = new Date().getTime()
+    const interval = setInterval(() => {
+      if (new Date().getTime() - startTime > 1500) {
+        clearInterval(interval)
+        return
+      }
+      setDice1(Math.floor(Math.random() * (max - min + 1)) + min)
+      setDice2(Math.floor(Math.random() * (max - min + 1)) + min)
+      setBounce('animate__animated animate__bounce')
+    }, 100)
+  }
+
+  return (
+    <>
+      <button onClick={() => rollOneDice(1, 6)}>Roll 1 Dice</button>
+      <button onClick={() => rollTwoDice(1, 6)}>Roll 2 Dice</button>
+
+      <div className='dice-wrapper'>
+        {dice1 && <div class={`dice dice-${dice1} ${bounce}`} />}
+
+        {dice2 && dice1 && <div class={`dice dice-${dice2} ${bounce}`} />}
+      </div>
+    </>
+  )
+}
+
+export default Dice
---
+++ src/components/Letter.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const Letter = () => {
+  const [letter, setLetter] = useState('')
+  const [bounce, setBounce] = useState('')
+
+  const letterGenerator = () => {
+    const startTime = new Date().getTime()
+    setBounce('')
+    const interval = setInterval(() => {
+      if (new Date().getTime() - startTime > 1200) {
+        clearInterval(interval)
+        return
+      }
+      setLetter(String.fromCharCode(65 + Math.floor(Math.random() * 26)))
+      setBounce('animate__animated animate__tada animate__delay-1s')
+    }, 50)
+  }
+  return (
+    <>
+      <div className='letter-container '>
+        <button onClick={letterGenerator}>New Letter</button>
+        <h1 className={`letter ${bounce}`}>{letter}</h1>
+      </div>
+    </>
+  )
+}
+
+export default Letter
