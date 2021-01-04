d46cfb49398b4fe80ecc60e00bc65afe3e5f1704
dnirns <daniel.irons@gmail.com>
2021-01-04 13:32:42 +0000

    footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,25 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='container'>
-      <h1 className='title'>DICER</h1>
-      <h3>A simple tool kit for playing group games on Zoom</h3>
-      <p style={{ fontSize: '.8em' }}>
-        (<em>other video conferencing software may be avalaible</em>)
-      </p>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <div className='container'>
+        <h1 className='title'>DICER</h1>
+        <h3>A simple tool kit for playing group games on Zoom</h3>
+        <p>
+          (<em>other video conferencing software may be avalaible</em>)
+        </p>
+        <p>
+          A little (work in progress) app to solve some needs I had while
+          playing group board games online.
+        </p>
+      </div>
+      <div className='footer'>
+        <p>
+          Developed by <a href='https://www.dnirns.com'>Daniel Irons</a>. View
+          the code on
+          <a href='https://github.com/dnirns/dicer'> GitHub</a>
+        </p>
+      </div>
     </div>
   )
 }
