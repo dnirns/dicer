8639b6f805f5317aaa554981f5f326c09613edf7
dnirns <daniel.irons@gmail.com>
2021-01-04 12:33:28 +0000

    responsive nav drawer finished

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import {
@@ -12,6 +13,8 @@ import MenuIcon from '@material-ui/icons/Menu'
 
 import { makeStyles } from '@material-ui/core/styles'
 
+import NavDrawer from './NavDrawer'
+
 const HideOnScroll = ({ children, window }) => {
   const trigger = useScrollTrigger({ target: window ? window() : undefined })
 
@@ -24,14 +27,13 @@ const HideOnScroll = ({ children, window }) => {
 
 const useStyles = makeStyles({
   root: {
-    background: '#5555553e',
+    background: '#222222',
     boxShadow: 'none',
     display: 'flex',
-
     alignItems: 'center',
   },
   icon: {
-    marginLeft: 'auto',
+    marginRight: 'auto',
   },
   menuLink: {
     fontFamily: 'Work Sans',
@@ -42,38 +44,49 @@ const useStyles = makeStyles({
 })
 
 const NavBar = () => {
+  const [sideDrawerOpen, setSideDrawerOpen] = useState(true)
   const classes = useStyles()
 
-  return (
-    <HideOnScroll>
-      <AppBar className={classes.root} position='sticky'>
-        <Toolbar variant='dense'>
-          <Box display={{ xs: 'none', sm: 'none', md: 'none' }}>
-            <div aria-haspopup='true'>
-              <IconButton edge='start' color='inherit' aria-label='menu'>
-                <MenuIcon fontSize='medium' />
-              </IconButton>
-            </div>
-          </Box>
-          <Box display={{ xs: 'block', sm: 'block', md: 'block' }}>
-            <Link to='/'>
-              <button className={classes.menuLink}>Home</button>
-            </Link>
-            <Link to='/roll-dice'>
-              <button className={classes.menuLink}>Roll Dice</button>
-            </Link>
-            <Link to='/random-letter'>
-              <button className={classes.menuLink}>Pick a Letter</button>
-            </Link>
-            <Link to='/timer'>
-              <button className={classes.menuLink}>Timer</button>
-            </Link>
-          </Box>
+  const handleCloseDrawer = () => {
+    setSideDrawerOpen(false)
+  }
 
-          <Box className={classes.icon}></Box>
-        </Toolbar>
-      </AppBar>
-    </HideOnScroll>
+  return (
+    <>
+      <NavDrawer
+        open={sideDrawerOpen}
+        closeDrawer={handleCloseDrawer}
+      />
+      <HideOnScroll>
+        <AppBar className={classes.root} position='sticky'>
+          <Toolbar variant='dense'>
+            <Box display={{ xs: 'block', sm: 'none', md: 'none' }}>
+              <div aria-haspopup='true'>
+                <IconButton className={classes.icon} onClick={() => setSideDrawerOpen(true)} edge='start' color='inherit' aria-label='menu'>
+                  <MenuIcon fontSize='medium' />
+                </IconButton>
+              </div>
+            </Box>
+            <Box display={{ xs: 'none', sm: 'block', md: 'block' }}>
+              <Link to='/'>
+                <button className={classes.menuLink}>Home</button>
+              </Link>
+              <Link to='/roll-dice'>
+                <button className={classes.menuLink}>Roll Dice</button>
+              </Link>
+              <Link to='/random-letter'>
+                <button className={classes.menuLink}>Pick a Letter</button>
+              </Link>
+              <Link to='/timer'>
+                <button className={classes.menuLink}>Timer</button>
+              </Link>
+            </Box>
+
+
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+    </>
   )
 }
 export default NavBar
---
+++ src/components/NavDrawer.js
@@ -0,0 +1,96 @@
+import { Link } from 'react-router-dom'
+
+import { makeStyles } from '@material-ui/core/styles'
+
+import {
+  List,
+  Divider,
+  ListItem,
+  Box,
+  ListItemText,
+  Button,
+  Drawer,
+} from '@material-ui/core'
+
+import CloseIcon from '@material-ui/icons/Close'
+
+const useStyles = makeStyles({
+  root: {
+    background: '#222222',
+    color: 'white',
+    padding: '14px',
+    textAlign: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  label: {
+    color: 'white',
+  },
+  closeBox: {
+    borderColor: 'rgba(0, 0, 0, 0.12)',
+    width: '56px',
+  },
+  list: {
+    width: '100vw',
+    background: '#222222',
+    color: 'white',
+    marginLeft: 'auto',
+  },
+})
+
+const SideSheet = ({ open, closeDrawer }) => {
+  const classes = useStyles()
+  return (
+    <>
+      <Drawer
+        // className={classes.root}
+        anchor='top'
+        open={open}
+        variant='temporary'
+        onClose={closeDrawer}
+      >
+        <Box className={classes.root} display='flex' flexDirection='row'>
+          <Button className={classes.label}>
+            <CloseIcon fontSize='large' onClick={closeDrawer} />
+          </Button>
+          <Divider orientation='vertical' flexItem />
+        </Box>
+
+        <List className={classes.list} onClick={closeDrawer}>
+          <ListItem className={classes.root}>
+            <Link to='/'>
+              <Button className={classes.label}>
+                <ListItemText primary='Home' />
+              </Button>
+            </Link>
+          </ListItem>
+
+          <ListItem className={classes.root}>
+            <Link to='/roll-dice'>
+              <Button className={classes.label}>
+                <ListItemText primary='Roll Dice' />
+              </Button>
+            </Link>
+          </ListItem>
+
+          <ListItem className={classes.root}>
+            <Link to='/random-letter'>
+              <Button className={classes.label}>
+                <ListItemText primary='Get a Letter' />
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem className={classes.root}>
+            <Link to='/timer'>
+              <Button className={classes.label}>
+                <ListItemText primary='Timer' />
+              </Button>
+            </Link>
+          </ListItem>
+        </List>
+      </Drawer>
+    </>
+  )
+}
+
+export default SideSheet
